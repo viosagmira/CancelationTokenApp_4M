49ba7fe97ad471fffcc51cb67c7f196e8eb625dc
Viosagmirt <viosagmirt@gmail.com>
2024-04-06 20:29:27 +0300

    AddedAsyncMethods

--- CancelationTokenApp_4M/Program.cs
+++ CancelationTokenApp_4M/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CancellationTokenSource cancellationToken = new CancellationTokenSource();
 
+        CancellationToken token = cancellationToken.Token;
+
+        var task = Task.Run(WelcomeAsync, token);
+
+        cancellationToken.Cancel();
+
+        //task.Wait();
+
+        Console.ReadLine();   // pause
+    }
+
+    private static async Task<int> FindMaxInArrayAsync(int[] array)
+    {
+        int res = await Task.Run(() => FindMaxInArray(array));
+
+        return res;
+    }
+
+    private static int FindMaxInArray(int[] array)
+    {
+        int max = array[0];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+            Thread.Sleep(1000);
+        }
+
+        return max;
+    }
+
+    private static async Task WelcomeAsync()
+    {
+        Console.WriteLine("Async Started...");
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("Async Stoped...");
+    }
+}
